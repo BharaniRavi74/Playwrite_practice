e06168e0f767c82e29c22bd17eec6b115478a7af
Bharani <bharaniravi74@gmail.com>
2025-07-14 21:57:42 +0530

    Hard Assert

---
+++ tests/Chapter2/09_Hard_asset.spec.ts
@@ -0,0 +1,36 @@
+//import Playwright
+
+import { test, expect } from "@playwright/test"
+
+//test case declaration
+test('Hard assetion', async ({ page }) => {
+    await test.step('open the url', async () => {
+    await page.goto('https://demoqa.com/text-box');
+    })
+
+    await test.step('is it on the screen - Visible and editable(editable by user)', async () => {
+    
+        await expect(page.locator('#userName')).toBeEditable();
+        await expect(page.locator('#userName')).toBeVisible();
+        await page.locator('#userName').fill('Bharani');
+
+        
+        
+    });
+    await test.step('Enabled like empty and enter the data', async () => {
+
+        await expect(page.locator('#userEmail')).toBeEmpty();
+         await expect(page.locator('#userEmail')).toBeEditable();
+        await page.locator('#userEmail').fill('bharaniravi789@gmail.com');
+        await page.locator('#currentAddress').fill('92,Mugil street,Arul velavannagar');
+         await page.locator('#permanentAddress').fill('92,Mugil street,Arul velavannagar');
+         //checking for editable to make it as failed
+         await expect(page.getByRole('button',{name:'Submit'})).toBeEnabled();
+         await page.getByRole('button',{name:'Submit'}).click();
+
+
+        
+    })
+      
+
+    });
